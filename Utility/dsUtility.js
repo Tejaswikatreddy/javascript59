bdd2b573e562876bd265df02ab4a9afbcf055e9f
Tejaswikatreddy <42598383+Tejaswikatreddy@users.noreply.github.com>
2018-09-14 19:13:25 +0530

    commit

--- Utility/dsUtility.js
+++ Utility/dsUtility.js
@@ -14,51 +14,38 @@ module.exports = {
      * @var {Object} readlineSync to take input from the user
      */
     unOrderList: function (arr, readlineSync) {
-        var index = readlineSync.question("enter the index where you want to search: ")//take the index where you want to search in the linked list
-        if (isNaN(index)) {//check if the input is number or string
-            console.log("Invalid input,try again..");
-            unOrderList(arr, readlineSync);
-        }
-
         for (var i = 0; i < arr.length; i++) {
             if (isNaN(arr[i]))
                 listObj.add(arr[i]);
+        }       
+        var ele = readlineSync.question("enter the element to be search: ");//enter the element you want to remove
+        if(isNaN(ele)==false){
+            console.log("enter string element to search,try again...")
+            var ele = readlineSync.question("enter the element to be search: ");
         }
-        var ele_at_in = listObj.get(index);
-        console.log("element at given index is: " + ele_at_in);
-        var search_ele = readlineSync.question("enter the element you want to search")
-        var search_result = listObj.search(search_ele)
-        if (search_result) {
-            console.log("element found")
-        }
-        var ele = readlineSync.question("enter the element to be removed: ");
         var index = listObj.indexOf(ele)
         if (index >= 0) {
-
-            listObj.remove(ele)
+            listObj.remove(ele)//remove the element if it is found
+            console.log("element found and removed")
         }
         else {
-            listObj.add(ele)
+            listObj.add(ele)//add the element if it is notr present in the list
+            console.log("element not found so added to the list")
         }
-        var pos = readlineSync.question("enter the pos where the ele shoould be enetered")
-        if (isNaN(pos)) {
-            console.log("Invalid input");
-            unOrderList(arr, readlineSync);
-        }
-        var item = readlineSync.question("enter the item to be entered")
-        listObj.insert(pos, item);
-
-        var lst_ele = listObj.pop();
-        console.log("element returned by pop is " + lst_ele)
+        
         var str = listObj.printlist();
-        fs.writeFileSync('file.txt', str.trim())
+        fs.writeFileSync('file.txt', str.trim())//save the modified list into the file
     },
+    /**
+     * @function expressionCheck() checks if the given expression have balanced paranthesis
+     * @var {String} string expression whichis to be checked
+     */
     expressionCheck: function (string) {
         for (i = 0; i < string.length; i++) {
             if (string[i] == '(')
-                stObj.push(string[i]);
+                stObj.push(string[i]);//invoke push method of stack
             else if (string[i] == ')')
-                stObj.pop();
+                stObj.pop();  //invoke pop method of the stack
         }
         if (stObj.isEmpty()) {
             console.log("balanced")
@@ -67,10 +54,14 @@ module.exports = {
             console.log("unbalenced")
         }
     },
+    /**
+     * @function palindrome() checks if the given string is palindrome or not
+     * @var {String} str the string to be checked
+     */
     palindromeCheck: function (str) {
         var flag = true;
         str = str.toLowerCase().trim();
-        str.split('')
+        str.split('')   //covert the string into an array
         for (var i = 0; i < str.length; i++) {
             if (str[i] != " ")
                 deObj.addRear(str[i]);//add characters to the queue
@@ -80,8 +71,8 @@ module.exports = {
                 break;
             }
 
-            var front = deObj.removeFront()
-            var rear = deObj.removeRear()
+            var front = deObj.removeFront() //invokes removeFRont method of dequeue
+            var rear = deObj.removeRear()   //invokes removeRear methof of dequeue
             if (front == rear)
                 flag = true;
             else {
@@ -97,12 +88,16 @@ module.exports = {
             console.log("string is not a palindrome")
         }
     },
+    /**
+     * @function cashCounter() performs transaction of the persons
+     * @var {Number} persons no of persons that are present in the queue intially
+     */
     cashCounter: function (persons, readlineSync) {
         for (var i = 0; i < persons; i++) {
-            qeObj.enqueue(i);
+            qeObj.enqueue(i);//enequeue the people into the queue
         }
         while (qeObj.isEmpty() > 0) {
-            var sel = readlineSync.question("enter your choice of 1.deposit 2.withdrawal 3.checkbalance-->")
+            var sel = readlineSync.question("enter your choice of 1.deposit 2.withdrawal 3.checkbalance-->")//takes the choice of transaction as input
             switch (sel) {
                 case '1': qeObj.cashdeposit(readlineSync);
                     break;
@@ -110,46 +105,49 @@ module.exports = {
                     break;
                 case '3': qeObj.checkbalance();
                     break;
-                default: console.log("invalid input,try again...")
+                default: console.log("invalid input,try next time...")
             }
-            qeObj.dequeue();
+            qeObj.dequeue();//after every transaction dequeu one person from the queue
             var add = readlineSync.question("Do you need to add persons to the queue Y:yes N:No-->")
             if (add == 'Y' || add == 'y') {
                 var number = readlineSync.question("enter number of persons u want to add: ")
-                for (var i = 0; i < number; i++) {
+                for (var i = 0; i < number; i++) {//every time check if there are persons to enter into the queue
                     qeObj.queue(i)
                 }
-
             }
         }
         if (qeObj.isEmpty() == 0) {
             console.log("Queue is empty..Transaction done ")
         }
     },
-    primeNumber: function (min, max) {
-        var count = 0, primeArr = [];
+    /**
+     * @function primeNumber() gives the prime Numbers from 0 to `1000`
+     */
+    primeNumber: function () {
+        var count = 0, primeArr = [],max=1000;
         if (max <= 1000) {
-            //  console.log(`prime numbers from  ${(min)} to ${(max)} are`)
-            for (var i = min; i <= max; i++) {
+            for (var i = 0; i <= max; i++) {
                 var flag = true;
                 for (var j = 2; j <= i / 2; j++) {
                     if (i % j == 0)
                         flag = false
                 }
                 if (flag == true) {
-                    // console.log(i)
                     count++;
-                    primeArr.push(i);
+                    primeArr.push(i);   //pushing all the prime numbers into an array
 
                 }
             }
         }
-        return primeArr;
+        return primeArr;    
     },
-    twoDArray: function (low, high) {
+    /**
+     * @function twoDArray () prints the prime numbers in the form of twoDArray
+     */
+    twoDArray: function () {
 
-        var primeArr = this.primeNumber(low, high);
-        var p = 100, prev = 0, j = 0, twoDArray = [];
+        var primeArr = this.primeNumber();
+        var p = 100, prev = 0, j = 0, twoDArray = [],max=1000;
         for (var i = 0; i < max / 100; i++)  //rows
         {
             var dArray = [], q = 0;
@@ -169,11 +167,21 @@ module.exports = {
 
 
         }
-        console.log(twoDArray)
+        for(var i=0;i<twoDArray.length;i++){
+            var str="";
+            for(var j=0;j<twoDArray[i].length;j++){
+                 str = str +" "+ (twoDArray[i])[j]
+            }
+                console.log(str)
+        }
+        
     },
-    anagram: function (low, high) {
+    /**
+     * @function anagram() give the prime numbers and anagrams from 0 to 1000
+     */
+    anagram: function () {
         var anArray = [], primeArray = [], twoDArray = [];
-        var primearr = this.primeNumber(low, high);
+        var primearr = this.primeNumber();
         for (var i = 0; i <= primearr.length; i++) {
             for (var j = i + 1; j <= primearr.length; j++) {
                 var s1 = "" + primearr[i];
@@ -189,10 +197,13 @@ module.exports = {
         }
         return anArray;
     },
-    anaAndPrime: function (low, high) {
+    /**
+     * @function anaAndPrime() prints the prime numbers and anagrams seperately in two dimensional array
+     */
+    anaAndPrime: function () {
         var anArray = [], primeArray = [], twoDArray = [];
-        var anArray = this.anagram(low, high);
-        var primearr = this.primeNumber(low, high);
+        var anArray = this.anagram();
+        var primearr = this.primeNumber();
         for (var p = 0; p < primearr.length; p++) {
             for (var q = 0; q < anArray.length; q++) {
                 if (primearr[p] === anArray[q]) {
@@ -206,50 +217,49 @@ module.exports = {
         }
         twoDArray.push(anArray);
         twoDArray.push(primeArray);
-        for (i = 0; i < twoDArray.length; i++) {
-            var str = "";
-            for (var j = 0; j < (twoDArray[i]).length; j++) {
-                str = str + " " + (twoDArray[i])[j]
-            }
-            console.log(str)
-        }
+        // for (i = 0; i < twoDArray.length; i++) {
+        //     var str = "";
+        //     for (var j = 0; j < (twoDArray[i]).length; j++) {
+        //         str = str + " " + (twoDArray[i])[j]
+        //     }
+        //     console.log(str)
+        // }
+        console.log(twoDArray)
 
     },
-    stackAnagram: function (low, high) {
+    /**
+     * @function stackAnagram() prints the anagrams in the reverse order using stack
+     */
+    stackAnagram: function () {
         var res = "";
-        var arr = this.anagram(low, high);
+        var arr = this.anagram();
         for (var i = 0; i < arr.length; i++) {
-            stObj.push(arr[i]);
+            stObj.push(arr[i]); //pushes the anagram array into the stack
         }
-        var stack = stObj.printstack();
+        var stack = stObj.printstack();//prints the stack
         console.log("stack is " + stack)
     },
-    queueAnagram: function (low, high) {
-        var arr = this.anagram(low, high)
+    /**
+     * @function queueAnagram() takes the anagrams into queue and prints the queue
+     */
+    queueAnagram: function () {
+        var arr = this.anagram()
         for (var i = 0; i < arr.length; i++) {
-            qeObj.enqueue(arr[i]);
+            qeObj.enqueue(arr[i]);//enqueues the anagrams into the queue
         }
-        var str = qeObj.printList();
+        var str = qeObj.printList();//prints the queue
         console.log("anagrams in queue are " + str);
     },
-    hash: function (array, readlineSync) {
-        var arr
-        // listObj.list();
-        // listObj.add(array);
-        //   var search=readlineSync.question("elemnt to search ")
-        // if(listObj.search(search)){
-        //     console.log("element found")
-        // }
-        // var str=listObj.printlist();
-        // console.log(str)
-        // console.log("size "+listObj.size());
-
-    },
+    /**
+     * @function prntCalendar() prints the calendar of the given month
+     * @var {Number} noOfDays is thenumber of days in the given month
+     * @var {Number} frstDay is the day of the first date of the month
+     */
     printCalendar: function (noOfDays, frstDay) {
         var calendar = [];
         var days = ["sun", "mon", "tue", "wed", "thu", "fri", "sat"]
         var week = [];
-        calendar.push(days)
+        calendar.push(days)  //push an array into an array
         var dates = 1;
         for (var i = 0; i < days.length; i++) {
             if (i < frstDay)
@@ -259,7 +269,7 @@ module.exports = {
                 dates++;
             }
         }
-        calendar.push(week)
+        calendar.push(week)     //push an array into an array
 
         while (dates <= noOfDays) {
             var week = [];
@@ -270,16 +280,19 @@ module.exports = {
                     week[i] = " "
             }
 
-            calendar.push(week)
+            calendar.push(week)        //push an array into an array
 
         }
         this.print(calendar, days)
     },
+    /**
+     * @function print() prints the calendar
+     */
     print: function (calendar, days) {
         for (var i = 0; i < calendar.length; i++) {
             var str = ""
             for (var j = 0; j < calendar[i].length; j++) {
-                str = str + " \t " + (calendar[i])[j]
+                str = str + " \t " + (calendar[i])[j]   //printing a 2D array in string format
 
             }
             console.log(str);
